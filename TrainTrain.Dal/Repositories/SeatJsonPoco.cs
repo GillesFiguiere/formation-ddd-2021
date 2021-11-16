8e9a29f675ffc582a2727f0ba500a46e9274ed3e
Gilles Figuière <me@gillesfiguiere.com>
2021-11-16 15:30:19 +0100

    Moved POCO to infra

---
+++ TrainTrain.Dal/Repositories/SeatJsonPoco.cs
@@ -0,0 +1,13 @@
+﻿namespace TrainTrain.Dal.Repositories
+{
+    public class SeatJsonPoco
+    {
+        public string booking_reference { get; set; }
+        public string seat_number { get; set; }
+        public string coach { get; set; }
+
+        public SeatJsonPoco()
+        {
+        }
+    }
+}
--- TrainTrain/Train.cs
+++ TrainTrain/Train.cs
@@ -12,36 +12,10 @@ namespace TrainTrain
             Seats = seats;
         }
 
-        public int GetMaxSeat()
-        {
-            return this.Seats.Count;
-        }
-
         public List<Seat> Seats { get; }
 
 
         public bool WillNotExceed70PercentReservation(int nbSeatRequested) =>
             Seats.Count(s => s.IsReserved()) + nbSeatRequested <= Math.Floor(ThreasholdManager.GetMaxRes() * Seats.Count);
-    }
-
-    public class TrainJsonPoco
-    {
-        public List<SeatJsonPoco> seats { get; set;  }
-
-        public TrainJsonPoco()
-        {
-            this.seats = new List<SeatJsonPoco>();
-        }
-    }
-
-    public class SeatJsonPoco
-    {
-        public string booking_reference { get; set; }
-        public string seat_number { get; set; }
-        public string coach { get; set; }
-
-        public SeatJsonPoco()
-        {
-        }
     }
 }
